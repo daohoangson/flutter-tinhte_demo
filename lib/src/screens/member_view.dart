daohoangson/flutter-tinhte_demo

commit 93ae7421140fd04bc555ec6fadf2684f78de1cfb

Use simple `AppBar` for member view and node view

lib/src/screens/member_view.dart
@@ -25,7 +25,7 @@ class _MemberViewScreenState extends State<MemberViewScreen> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: buildAppBar(
+        appBar: AppBar(
           title: Text(user.username),
         ),
         body: NotificationListener<ScrollNotification>(

lib/src/screens/node_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tinhte_api/navigation.dart' as navigation;
 
-import '../widgets/app_bar.dart';
 import '../widgets/navigation.dart';
 
 class NodeViewScreen extends StatelessWidget {
@@ -11,7 +10,7 @@ class NodeViewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: buildAppBar(
+        appBar: AppBar(
           title: Text(element.node.title),
         ),
         body: NavigationWidget(path: element.links.subElements),

commit 05acc94bd5f41f9137d4a838cd40fab754ba07d7

Remove unused import (caused by 93ae742)

lib/src/screens/member_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tinhte_api/user.dart';
 
 import '../widgets/user/member_view_header.dart';
-import '../widgets/app_bar.dart';
 import '../widgets/threads.dart';
 import '../api.dart';
 import 'search/thread.dart';