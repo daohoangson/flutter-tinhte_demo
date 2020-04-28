e14ec4838a56916a782bcaa7acf09562a8b8b9ba
Dao Hoang Son <daohoangson@gmail.com>
2020-04-28 15:55:58 +0700

    Fix TestFlight build not being distributed

--- tool/release-beta.sh
+++ tool/release-beta.sh
@@ -9,6 +9,12 @@ if [ -z "$FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD" ]; then
   exit 1
 fi
 
+if [ -z "$PILOT_BETA_APP_REVIEW_INFO" ]; then
+  # export PILOT_BETA_APP_REVIEW_INFO='{"contact_email":"admin@domain.com","contact_first_name":"First","contact_last_name":"Last","contact_phone":"0123456789","demo_account_name":"username","demo_account_password":"password"}'
+  echo 'Env var PILOT_BETA_APP_REVIEW_INFO must be set' >&2
+  exit 1
+fi
+
 cd "$_pwd"
 flutter build appbundle --release
 cd android && fastlane beta
