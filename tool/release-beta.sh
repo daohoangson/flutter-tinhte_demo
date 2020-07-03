bd92d1c271213443a63933f6b64df4e31bd7b77b
Dao Hoang Son <daohoangson@gmail.com>
2020-07-03 10:22:35 +0700

    Switch to use `FASTLANE_SESSION` instead of app password

--- tool/release-beta.sh
+++ tool/release-beta.sh
@@ -4,8 +4,13 @@ set -eo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 _pwd=$( pwd )
 
-if [ -z "$FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD" ]; then
-  echo 'Env var FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD must be set' >&2
+if [ -z "$FASTLANE_SESSION" ]; then
+  # docker run --rm -it -e LC_ALL=en_US.UTF-8 -e LANG=en_US.UTF-8 -v $PWD:$PWD -w $PWD/ios ruby /bin/bash
+  #   gem install fastlane -NV
+  #   fastlane spaceauth
+  #   exit
+  # export FASTLANE_SESSION='---\n- !ruby/object:HTTP::Cookie\n...'
+  echo 'Env var FASTLANE_SESSION must be set' >&2
   exit 1
 fi
 
