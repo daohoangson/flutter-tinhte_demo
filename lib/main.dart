90a862ee60fc614a20473b781c77a4bfa914a80a
Dao Hoang Son <daohoangson@gmail.com>
2021-06-03 23:55:27 +0700

    Allow popping back from uni link / push notification

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,10 @@ void main() async {
     ]);
 
     String initialPath;
-    bool isPushNotificationLaunch = false;
+    Widget defaultWidget;
     if (values[2] != null) {
       initialPath = values[2];
-      isPushNotificationLaunch = true;
+      defaultWidget = NotificationListScreen();
     } else if (values[3] != null) {
       initialPath = values[3];
     }
@@ -48,8 +48,7 @@ void main() async {
       home: initialPath != null
           ? OnLaunchWidget(
               initialPath,
-              defaultWidget:
-                  isPushNotificationLaunch ? NotificationListScreen() : home,
+              defaultWidget: defaultWidget,
               fallbackWidget: home,
             )
           : home,
--- lib/src/widgets/on_launch_widget.dart
+++ lib/src/widgets/on_launch_widget.dart
@@ -9,16 +9,60 @@ class OnLaunchWidget extends StatefulWidget {
 
   OnLaunchWidget(this.path, {this.defaultWidget, this.fallbackWidget, Key key})
       : assert(path != null),
-        assert(defaultWidget != null),
         assert(fallbackWidget != null),
         super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _OnLaunchState();
+  State<OnLaunchWidget> createState() => _OnLaunchState();
 }
 
 class _OnLaunchState extends State<OnLaunchWidget> {
-  var _fallback = false;
+  var _cameBack = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      // request builder in the next frame
+      await Navigator.push(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => _BuilderWidget(
+            widget.path,
+            defaultWidget: widget.defaultWidget,
+          ),
+          transitionDuration: Duration.zero,
+          reverseTransitionDuration: Duration.zero,
+        ),
+      );
+
+      if (mounted) {
+        setState(() => _cameBack = true);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext _) {
+    if (_cameBack) return widget.fallbackWidget;
+    return SizedBox.shrink();
+  }
+}
+
+class _BuilderWidget extends StatefulWidget {
+  final Widget defaultWidget;
+  final String path;
+
+  _BuilderWidget(this.path, {this.defaultWidget, Key key})
+      : assert(path != null),
+        super(key: key);
+
+  @override
+  State<_BuilderWidget> createState() => _BuilderState();
+}
+
+class _BuilderState extends State<_BuilderWidget> {
   Future<Widget> _future;
 
   @override
@@ -31,17 +75,32 @@ class _OnLaunchState extends State<OnLaunchWidget> {
       defaultWidget: widget.defaultWidget,
     ).catchError((error) async {
       await showApiErrorDialog(context, error);
-      setState(() => _fallback = true);
+      return null;
+    }).then((widget) {
+      if (widget == null && mounted) {
+        // no widget could be built:
+        // 1. Bad link
+        // 2. API errors
+        // -> go back to OnLaunchWidget, it will render the fallback widget
+        Navigator.of(context).pop();
+      }
+
+      return widget;
     });
   }
 
   @override
-  Widget build(BuildContext _) => _fallback
-      ? widget.fallbackWidget
-      : FutureBuilder<Widget>(
-          builder: (__, snapshot) => snapshot.hasData
-              ? snapshot.data
-              : const Center(child: CircularProgressIndicator()),
-          future: _future,
-        );
+  Widget build(BuildContext _) => FutureBuilder<Widget>(
+        builder: (__, snapshot) {
+          final widget = snapshot.data;
+          if (widget == null) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+
+          return widget;
+        },
+        future: _future,
+      );
 }
