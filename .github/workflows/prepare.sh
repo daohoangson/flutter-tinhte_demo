299ccbf1a049444799b85e3dfaa625ffff02eed0
Dao Hoang Son <daohoangson@gmail.com>
2023-08-13 23:08:59 +0700

    Remove `yq` installation completely

--- .github/workflows/prepare.sh
+++ .github/workflows/prepare.sh
@@ -2,22 +2,19 @@
 
 set -eo pipefail
 
-# Unlock git-crypt
+# 1. Unlock git-crypt
+# We could not use the existing GitHub action because it doesn't support Windows
+# https://github.com/sliteteam/github-action-git-crypt-unlock/blob/master/entrypoint.sh
 if ! command -v git-crypt &>/dev/null; then
   brew install git-crypt
 fi
-# https://github.com/sliteteam/github-action-git-crypt-unlock/blob/master/entrypoint.sh
 echo "${GIT_CRYPT_KEY}" | base64 -d >./git-crypt-key
 file lib/config.encrypted.dart
 git-crypt unlock ./git-crypt-key
 file lib/config.encrypted.dart
 rm ./git-crypt-key
 
-# Extract config from pubspec.yaml
-if ! command -v yq &>/dev/null; then
-  brew install yq
-fi
-
+# 2. Patch app version with monotonic build number
 _appVersion=$(yq e '.version' pubspec.yaml)
 echo "_appVersion=${_appVersion}"
 _appVersionWithBuildNumber=$(printf '.version="%s+%d"' "${_appVersion}" "${GITHUB_RUN_NUMBER}${GITHUB_RUN_ATTEMPT}")
@@ -25,10 +22,10 @@ echo "_appVersionWithBuildNumber=${_appVersionWithBuildNumber}"
 yq --inplace e "$_appVersionWithBuildNumber" pubspec.yaml
 cat pubspec.yaml
 
+# 3. Expose Flutter version
 if command -v wslpath &>/dev/null; then
   _wslGithubEnv=$(wslpath -u "$GITHUB_ENV")
   export "GITHUB_ENV=${_wslGithubEnv}"
 fi
-
 _flutterVersion=$(yq e '.environment.flutter' pubspec.yaml)
 echo "FLUTTER_VERSION=${_flutterVersion}" | tee -a $GITHUB_ENV
